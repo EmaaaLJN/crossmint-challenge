da8197b19145a796730ffa6853936b45e9a62320
Emanuel A. Luján <ema.luj+dev@gmail.com>
2023-09-15 22:17:27 -0300

    add column direction to comeths

--- spec/factories/comeths.rb
+++ spec/factories/comeths.rb
@@ -10,6 +10,7 @@ end
 # Table name: comeths
 #
 #  id         :bigint           not null, primary key
+#  direction  :integer          default(0)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
